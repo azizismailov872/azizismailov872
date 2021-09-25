9c0aff6cc0b54d74cc9965254a8e75cd1c540df5
Aziz Ismailov <azizismailov872872@gmail.com>
2021-09-25 17:44:00 +0600

    Slider project

---
+++ myslider/js/slider.js
@@ -0,0 +1,46 @@
+const upBtn = document.querySelector('.up-button');
+
+const downBtn = document.querySelector('.down-button');
+
+const sideBar = document.querySelector('.sidebar');
+
+const mainSlide = document.querySelector('.main-slide');
+
+const container = document.querySelector('.container');
+
+const slidesCount = mainSlide.querySelectorAll('.slide').length;
+
+let activeSlideIndex = 0;
+
+sideBar.style.top = `-${(slidesCount -1) * 100}vh`;
+
+upBtn.addEventListener('click',(event) => {
+	changeSlide('up');
+});
+
+downBtn.addEventListener('click',(event) => {
+	changeSlide('down');
+});
+
+function changeSlide(direction){
+	if(direction === 'up')
+	{
+		activeSlideIndex++
+		if(activeSlideIndex === slidesCount)
+		{
+			activeSlideIndex = 0;
+		}
+	}else if(direction === 'down')
+	{
+		activeSlideIndex--
+		if(activeSlideIndex < 0){
+			activeSlideIndex = slidesCount - 1;
+		}
+	}
+
+	const height = container.clientHeight;
+
+	mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
+
+	sideBar.style.transform = `translateY(${activeSlideIndex * height}px)`;
+}
